75d6227cbf7b4a196fd766f8dca23a275838fc8d
michalmazurdev <m.mazur40@gmail.com>
2023-07-04 13:46:54 +0200

    initial set up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import css from './App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1 className={css.heading}>Pracownicy posiadlości Pięknej i Bestii</h1>
     </div>
   );
 };
